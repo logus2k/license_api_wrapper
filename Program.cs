30f40e5a76d5c6d111eb76536b3013d23c17ddea
António Cruz <logus2k@gmail.com>
2022-04-20 01:48:55 +0100

    Commit after location changes

--- Program.cs
+++ Program.cs
@@ -22,7 +22,7 @@ namespace LicenseApiWrapper
                 app.UseSwaggerUI();
             }
 
-            app.UseHttpsRedirection();
+            // app.UseHttpsRedirection();
 
             app.UseAuthorization();
 
